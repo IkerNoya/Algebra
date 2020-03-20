516faf5962c9be4172f7bd5ce7a406b08c37b79e
Iker <ikernoya4@gmail.com>
2020-03-20 11:19:01 -0300

    Completed scalar functions

--- New Unity Project/Assets/Scripts/MathDebbuger/Tests/Tester.cs
+++ New Unity Project/Assets/Scripts/MathDebbuger/Tests/Tester.cs
@@ -13,12 +13,18 @@ public class Tester : MonoBehaviour
         Vec3 test2 = new Vec3(7.0f, 7.0f, 7.0f);
         Vec3 resta = new Vec3(test - test2);
         Vec3 invertir = new Vec3(-resta);
+        Vec3 mult = new Vec3(test * 2.0f);
+        Vec3 mult2 = new Vec3(2.0f * test);
         VectorDebugger.AddVector(test, Color.red, "red one");
         VectorDebugger.AddVector(test2, Color.blue, "blue one");
         VectorDebugger.AddVector(resta, Color.green, "green one");
         VectorDebugger.AddVector(invertir, Color.yellow, "yellow one");
+        VectorDebugger.AddVector(mult, Color.gray, "gray one");
+        VectorDebugger.AddVector(mult2, Color.black, "black one");
         Debug.Log((resta).ToString());
         Debug.Log((invertir).ToString());
+        Debug.Log((mult).ToString());
+        Debug.Log((mult2).ToString());
         VectorDebugger.EnableEditorView();
 
     }
